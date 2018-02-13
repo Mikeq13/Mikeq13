5b25e771f5fafc228a4e5a97e9b11b298ca4f2f2
Mike <13jmmq@gmail.com>
2018-02-12 21:32:06 -0700

    integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.3'
 
 gem 'bootstrap', '~> 4.0.0'
+
+gem 'devcamp_view_tool',  git: 'https://github.com/Mikeq13/devcamp_view_tool'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,17 @@ class ApplicationController < ActionController::Base
   include SetSource
   include DefaultPageContent
 
-  before_filter :set_title
   def set_title
   	@page_title = "Devcamp Portfolio | My Portfolio Website"
   end
 end
+
+module DevcampViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
+end
+
+#before_filter :set_title
