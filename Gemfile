9648a5b66a90d8eb78154c30c6d4c3880c534b1f
Mike <13jmmq@gmail.com>
2018-02-19 23:18:50 -0700

    integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -54,3 +54,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.3'
+gem 'Quintero_view_tool', git: 'https://github.com/Mikeq13/Quintero_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
 			content_tag(:p, greeting, class: "source-greeting")
 		end
 	end
+
+	def copyright_generator
+		QuinteroViewTool::Renderer.copyright 'Michael Quintero', 'All Rights Reserved'
+	end
 end
