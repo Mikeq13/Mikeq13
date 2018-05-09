8e4ae8a52033f5d38df8e58a54a5ead66647f7b3
Mike <13jmmq@gmail.com>
2018-05-08 19:37:39 -0600

    fixed error with ben

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,26 @@
+User.create!(
+  email: "test@test.com",
+  password: "asdfasd",
+  password_confirmation: "asdfasd",
+  name: "Admin User",
+  roles: "site_admin"
+  )
+  
+  puts "1 Admin user created"
+  
+User.create!(
+  email: "test2@test.com",
+  password: "asdfasd",
+  password_confirmation: "asdfasd",
+  name: "Regular User",
+  roles: "user"
+  )
+  
+puts "1 regular user created"
+
+
+
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
